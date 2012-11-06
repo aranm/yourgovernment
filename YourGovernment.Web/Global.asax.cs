e267350bf17a3e3ec060e4592d51fbb06826cebe
Aran Mulholland <aranmulholland@gmail.com>
2012-11-06 18:42:23 +1100

    Added autofac for dependency injection, configured for use with standard controllers and rest endpoints

--- YourGovernment.Web/Global.asax.cs
+++ YourGovernment.Web/Global.asax.cs
@@ -6,6 +6,10 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using YourGovernment.Web.Controllers;
 
 namespace YourGovernment.Web {
    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
@@ -19,6 +23,19 @@ namespace YourGovernment.Web {
          FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
          RouteConfig.RegisterRoutes(RouteTable.Routes);
          BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+         var builder = new ContainerBuilder();
+         builder.RegisterControllers(typeof(HomeController).Assembly);
+         builder.RegisterApiControllers(typeof(ValuesController).Assembly);
+
+         //Registrations
+         //builder.RegisterType<DateTimeProvider>().As<IDateTimeProvider>();
+
+         var container = builder.Build();
+
+         DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+         GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
       }
    }
 }
